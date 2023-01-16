b1b6e192452bfc41b1d46f49175787d82cce2934
codingrishant11 <122806586+codingrishant11@users.noreply.github.com>
2023-01-16 22:08:43 +0530

    add initial rishant website files

---
+++ port2.js
@@ -0,0 +1,10 @@
+/*var circle = document.getElementById("circle");
+var hhh = document.getElementById("hhh");
+
+var rotatevalue = circle.style.transform;
+var rotatesum;
+hhh.onclick = function(){
+  rotatesum = rotatevalue + "rotate(180deg)";
+  circle.style.transform = rotatesum;
+  rotatevalue = rotatesum;
+}*/
